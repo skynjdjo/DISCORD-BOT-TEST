533435b00f215a7defae82749782cf5f092ea566
skynjdjo <51291085+skynjdjo@users.noreply.github.com>
2021-07-24 15:50:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const port = 5000
 app.get('/', function(request, response) {
     var result = 'App is running'
     response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+}).listen(port, function() {
+    console.log('App is running, server is listening on port ', ${port});
 });
 //////////////////////////////////////////////
 
